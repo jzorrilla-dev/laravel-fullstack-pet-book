a01216bbaa0549ae676a05bfa2c296b8d111e806
Juan Zorrilla <emiliozorrilla1994jz@gmail.com>
2025-10-16 17:41:40 -0300

    Se modificaron los archivos TrustProxies.php y AppServiceProvider.php para corregir el problema de la generación de URLs con Laravel.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Esto fuerza a Laravel a generar todas las URLs con 'https'
+        // Lo aplicamos en 'production' (Render) para evitar contenido mixto.
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
