a654b2632e909e298d0252e53089162737d0be19
Juan Zorrilla <emiliozorrilla1994jz@gmail.com>
2026-03-31 17:06:31 -0300

    security: eliminar axios del proyecto por vulnerabilidad supply chain

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,13 +1,16 @@
 /**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
+ * Laravel Vite Plugin configuration
+ * 
+ * This file configures the JavaScript environment for the application.
+ * Alpine.js is used for frontend interactivity.
  */
 
-import axios from 'axios';
-window.axios = axios;
+// Import Alpine.js
+import Alpine from 'alpinejs';
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.Alpine = Alpine;
+
+Alpine.start();
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
